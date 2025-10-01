d5892b8b946bada979e5eb7175fa8b5e8b0dea7b
Brian Oduory <brianmubweka17@gmail.com>
2025-10-01 19:49:45 +0300

    api changes

--- my-app/src/api/axios.js
+++ my-app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api", // relative path so Vite forwards to Flask backend
+  baseURL: 'http://127.0.0.1:5000/api'
 });
 
 export default api;
